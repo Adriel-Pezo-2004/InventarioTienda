bc8d9d576fd7605a8cd183f7ac45d891118c5878
Adriel-Pezo-2004 <adriefape@gmail.com>
2023-11-06 07:45:09 -0500

    env

--- Productos/models.py
+++ Productos/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Producto(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True)
     nombre = models.CharField(max_length = 30)
     descripcion = models.Field(max_length = 60)
-    precio = models.DecimalField(max_digits = 10, decimal places = 2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+
